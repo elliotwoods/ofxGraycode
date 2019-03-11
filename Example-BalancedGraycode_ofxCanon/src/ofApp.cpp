16ef57387c58b5c92c8d96c5fd839dd6abf4c04a
Elliot Woods <elliot@kimchiandchips.com>
2019-03-11 19:41:59 +0900

    progress with the example

--- Example-BalancedGraycode_ofxCanon/src/ofApp.cpp
+++ Example-BalancedGraycode_ofxCanon/src/ofApp.cpp
@@ -3,34 +3,107 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	this->gui.init();
+
 	this->payload = make_shared<ofxGraycode::Payload::BalancedGraycode>();
 	this->payload->init(1920, 1080);
 	this->encoder.init(this->payload);
 	this->decoder.init(this->payload);
 
-	ofPixels message;
-	uint32_t index = 0;
-	while (this->encoder >> message) {
-		index++;
-		this->encoder >> message;
-		cout << "Frame " << index << endl;
-		this->decoder << message;
+	auto devices = ofxCanon::listDevices();
+	if (devices.empty()) {
+		ofSystemAlertDialog("Please attach a Canon camera and try again");
+		ofExit();
 	}
+
+	this->device = devices.front();
+	this->device->open();
+
+	auto strip = this->gui.addStrip();
+	{
+		strip->setCellSizes({ -1, 300 });
+
+		{
+			auto grid = ofxCvGui::Panels::Groups::makeGrid();
+			grid->add(ofxCvGui::Panels::makeImage(this->message, "Message"));
+			grid->add(ofxCvGui::Panels::makeImage(this->photo, "Photo"));
+			grid->add(ofxCvGui::Panels::makeImage(this->decoder.getProjectorInCamera(), "Projector in camera"));
+			grid->add(ofxCvGui::Panels::makeImage(this->decoder.getCameraInProjector(), "Camera in projector"));
+
+			strip->add(grid);
+		}
+
+		{
+			auto widgets = ofxCvGui::Panels::makeWidgets();
+			strip->add(widgets);
+
+			widgets->addFps();
+			widgets->addMemoryUsage();
+
+			widgets->addButton("Render output files", [this]() {
+				this->encoder.init(this->payload);
+				int index = 0;
+				while (this->encoder >> this->message) {
+					index++;
+					auto indexString = ofToString(index);
+					while (indexString.size() < 3) {
+						indexString = "0" + indexString;
+					}
+					ofSaveImage(this->message, indexString + ".png");
+				}
+			});
+
+			widgets->addButton("Take photo", [this]() {
+				this->takePhoto();
+			}, ' ');
+
+			widgets->addLiveValue<int>("Decoder frame", [this]() {
+				return this->decoder.getFrame();
+			});
+
+			widgets->addButton("Save...", [this]() {
+				this->decoder.saveDataSet();
+			});
+
+			widgets->addButton("Load...", [this]() {
+				this->decoder.loadDataSet();
+			});
+
+			widgets->addEditableValue<int>("Threshold [/255]", [this]() {
+				return this->decoder.getThreshold();
+			}, [this](string valueString) {
+				this->decoder.setThreshold(ofToInt(valueString));
+			});
+		}
+	}
+	
+
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
 	this->decoder.update();
+	this->device->update();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	this->decoder.draw(0, 0);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::takePhoto() {
+	auto photoResult = this->device->takePhoto();
+	if (photoResult) {
+		ofLoadImage(this->photo, *photoResult.encodedBuffer);
+		this->photo.update();
+		this->decoder << this->photo;
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+	
 }
 
 //--------------------------------------------------------------
--- Example-BalancedGraycode_ofxCanon/src/ofApp.h
+++ Example-BalancedGraycode_ofxCanon/src/ofApp.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxCvGui.h"
 #include "ofxGraycode.h"
+#include "ofxCanon.h"
 
 class ofApp : public ofBaseApp{
 
@@ -9,6 +11,8 @@ class ofApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
+		
+		void takePhoto();
 
 		void keyPressed(int key);
 		void keyReleased(int key);
@@ -22,7 +26,15 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
-		shared_ptr<ofxGraycode::Payload::BalancedGraycode > payload;
+		ofxCvGui::Builder gui;
+
+		shared_ptr<ofxGraycode::Payload::BalancedGraycode> payload;
 		ofxGraycode::Encoder encoder;
 		ofxGraycode::Decoder decoder;
+		ofParameter<float> threshold;
+
+		ofImage message;
+		ofImage photo;
+
+		shared_ptr<ofxCanon::Device> device;
 };
